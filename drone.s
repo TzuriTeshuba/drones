0a37bc39c042957f954d66a33cd6b69fdae31392
Tzuri <teshuba@post.bgu.ac.il>
2020-06-16 23:24:38 +0300

    little progress

--- drone.s
+++ drone.s
@@ -1,15 +1,98 @@
 section .rodata
+    %define droneSize 20
+    %define xOffset 0
+    %define yOffset 2
+    %define speedOffset 4
+    %define angleOffset 6
+    %define scoreOffset 8
+    %define isAliveOffset 12
+    %define pidOffset 16
 
 section .bss
 
 section .data
 
 section .text
-    global storeId
-;;
-;;;;;;;;;;;;;;;;; 
-;;
-droneRun:
+    extern getDrone
+    extern getRandomNumber
+    
+
+%macro getSelf 0
+    mov eax, [ebp-4]    ;eax = index of self
+    getDrone eax
+%endmacro
+
+%macro getSpecPtr 1
+    mov ecx, %1
+    getSelf
+    add eax, ecx
+%endmacro    
+
+;;arg is spec to update with random
+%macro update16BitSpec 1
+    mov eax, %1
+    getSpecPtr eax
+    push eax                ;top of stack = specAdrs
+    call getRandomNumber    ;random in eax (ax)
+    pop ebx                 ;ebx = specAdrs 
+    mov word[ebx],ax        ;spec = random
+%endmacro 
+
+;;TODO: 
+;; 1) check carry flag and clamp to bound if needed
+;; 2) divide random by 10 before adding (for sake of common unit ratios)
+%macro updateSpeed 0
+    mov eax, speedOffset
+    getSpecPtr eax
+    push eax                ;top of stack = specAdrs
+    call getRandomNumber    ;random in eax (ax)
+    ;;divide random by 10 before adding!!
+    pop ebx                 ;ebx = specAdrs 
+    add word[ebx],ax        ;speed = speed + random
+    ;;check for overflow!!!
+%endmacro 
+
+;;TODO: 
+;; 1) check carry flag and clamp to bound if needed
+;; 2) divide random by 6 before adding (for sake of common unit ratios)
+%macro updateAngle 0
+    mov eax, angleOffset
+    getSpecPtr eax
+    push eax                ;top of stack = specAdrs
+    call getRandomNumber    ;random in eax (ax)
+    ;;divide random by 6!!
+    pop ebx                 ;ebx = specAdrs 
+    add word[ebx],ax        ;speed = speed + random
+    ;;check for overflow!!!
+%endmacro 
+
+
+
+;;TODO: Implement!
+%macro move 0
+    getSelf
+    mov ebx, 0
+    mov bx, [eax+xOffset]
+    mov ecx, 0
+    mov cx, [eax+yOffset]
+    
+    ;;update position
+%endmacro
+
+
+%macro updateSpecs 0
+    getRandomNumber     ;eax = random 16 bits
+    push eax
+    getSpecPtr xOffset
+    mov ebx, eax
+
+
+%endmacro
+
+
+droneTurn:
     ;;index stored at ebp - 4
+    getSelf     ;eax = self
+
     
 
--- scheduler.s
+++ scheduler.s
@@ -1,5 +1,6 @@
 section .rodata
     droneSize: dd 7 ;pid(dword),x,y,speed,angle,score
+    argsFormat:
 section .bss
     random: resw 1
     N: resd 1   ;initial number of drones
@@ -7,6 +8,7 @@ section .bss
     K: resd 1   ;how many drone steps between game board printings  
     d: resd 1   ;(float) maximum distance that allows to destroy a target
     drones: resd 1    ;pointer to drone array
+    temp: resd 1
 
 section .data
     currDroneId: dd 0
@@ -17,18 +19,28 @@ section .text
     global getDrones
     global getDrone
     global getN
+    extern sscanf
+
+main:
+    mov eax, [esp + 4] ;eax holds int argc
+    mov ebx, [esp + 8] ;ebx holds char** argv
+    cmp eax, 6  ;should be 6 args (progName, N, R, K, d, seed)
+    jne endMain
+
+
+
+    endMain:
 
 getN:
     mov eax, [N]
     ret
-
-
 
 getDrones:
     mov eax, [drones]
     ret
 
 ;assumes dword of index was pushed
+;SHOULD NOT USE ecx
 getDrone:
     call getDrones
     mov ebx, eax
